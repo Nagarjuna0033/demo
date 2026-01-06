7448ea3de106789f0658aa7e40b1562048c8b27d
Nagarjuna <nagarjunabanda4@gmail.com>
2026-01-06 10:54:46 +0530

    refactor: move UI theme files to core package

--- app/src/main/java/com/arjun/demo/core/ui/theme/Theme.kt
+++ app/src/main/java/com/arjun/demo/core/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.arjun.demo.ui.theme
+package com.arjun.demo.core.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
